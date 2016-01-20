66983da3be87c7bac4a65c2cc868bbfb4a8e85f2
palmnet <yasushi.sugino@gmail.com>
2016-01-20 18:47:50 +0900

    alarm list

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var d3 = require("d3");
 var redis = require("redis"),
   client1 = redis.createClient(),
   client2 = redis.createClient(),
-  client3 = redis.createClient();
+  client3 = redis.createClient(),
+  client4 = redis.createClient();
 
 app.use(express.static('public'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
@@ -33,14 +34,12 @@ app.get('/dashboard', function(req, res){
 app.get('/area', function(req, res){
     res.sendfile('area.html');
 });
+
 app.get('/init_alarms', function(req, res){
-  client2.zrange("alarms",0 ,-1, function(err, data) {
+  client2.zrevrange("alarms",0 ,10, function(err, data) {
     if(err) {
-      console.log(err);
       return;
     }
-    console.log(data);
-
     var parsed = data.map(function(elem) {
       return JSON.parse(elem)
     });
@@ -49,23 +48,14 @@ app.get('/init_alarms', function(req, res){
 });
 
 
-
-client1.subscribe("spark-alarm");
-client1.on("message", function (channel, message) {
-	client2.incr("total-alarms");
-	client2.get("total-alarms", function(err, replies) {
-  });
-});
-
-
-client3.subscribe("node-alarm");
-
+client4.subscribe("send-alarm");
+    
 io.sockets.on('connection', function(socket) {
 		socket.on('disconnect', function () {
 			io.emit('user disconnected');
-      console.log('disconnected!');
 		});
   
+    /*
     socket.emit('greeting', {message: 'hello'}, function (data) {
       console.log('result: ' + data);
     });
@@ -76,36 +66,34 @@ io.sockets.on('connection', function(socket) {
       });
     });
 
-
     socket.on('reply', function(data) {
       console.log('reply: ' + data);
     });
+    */
 
-    client1.on("message", function (channel, message) {
-      console.log("channel " + channel + " message " + message);
-
-      client2.get("total-alarms", function(err, replies) {
-        socket.emit("total-alarms", replies, function(data) {
-          console.log("total-alarms-reply:" + data);
-        });
+    client4.on("message", function (channel, message) {
+      var parsed = JSON.parse(message)
+      socket.emit("send-alarm", parsed, function(data) {
       });
-      socket.emit("publish", message, function(data) {
+
+      var high = 100, low = 0;
+      var val = Math.random() * (high - low) + low;
+
+      socket.emit("publish", val, function(data) {
         console.log('publish: ' + data);
       });
-
     });
 
+    /*
     client3.on("message", function (channel, message) {
-
-      console.log("channel " + channel + " message " + message);
       socket.emit("alarm", message, function(data) {
         console.log('alarm: ' + data);
       });
     });
+    */
 });
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
 });
-
 
